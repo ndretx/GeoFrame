5a8f86996e224ee31f7ad1eb28052b176f38cba6
ndretx <andrechteixeira@gmail.com>
2023-08-08 05:53:27 -0300

    iniicio chat

--- src/pages/chat-page.tsx
+++ src/pages/chat-page.tsx
@@ -1,7 +1,12 @@
-import { useEffect } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, FlatList, StyleSheet, Text, Image } from "react-native";
+import chatEntity from "../entity/chat-entity";
 
-export default function chatPage({navigation, route}) {
+export default function chatPage({ navigation, route }) {
+    const [author, setAuthor] = useState('');
+    const [messages, setMessages] = useState<chatEntity[]>([ ])
+    const [listRef, setListRef] = useState (null);
+    const [mensages, setMessages] = useState('')
     useEffect(() => {
 
 
@@ -9,10 +14,24 @@ export default function chatPage({navigation, route}) {
 
 
     return (
-        <View style={ styles.container } >
-            <View style={ styles.containerMessage }>
-            <View style={{backgroundColor:'white', width : 200}}>
-                <Image source={{ uri: ""}} />
+        <View style={styles.container} >
+            <FlatList
+                ref={(ref)=> {setListRef(ref)}}
+                data={messages}
+                renderItem={({ item }) => {
+                    return (
+                        item.sender === author ?
+                            <View>
+                                {/* Area de mensagem recebida */}
+                            </View> :
+                            <View>
+                                {/* Area de mensagem enviada */}
+                            </View>
+                    )
+                }} />
+            <View style={styles.containerMessage}>
+                <View style={{ backgroundColor: 'white', width: 200 }}>
+                    <Image source={{ uri: "" }} />
                     <Text> Chat</Text>
                 </View>
             </View>
@@ -23,6 +42,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+        justifyContent: 'center',
 
     },
     containerMessage: {
